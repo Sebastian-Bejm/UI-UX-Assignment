1b68eb2a8978f12041c9e38d58ecbf1446af814c
Sebastian Bejm <125sbejm@gmail.com>
2025-07-28 12:18:13 -0700

    added project detail modals

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// JavaScript to open/close modal
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "flex";
+    }
+}
+  
+function closeModal(button) {
+    const modal = button.closest(".modal");
+    if (modal) {
+        modal.style.display = "none";
+    }
+}  
+
 function toggleDetails(card) {
     card.classList.toggle('active');
 }
-
